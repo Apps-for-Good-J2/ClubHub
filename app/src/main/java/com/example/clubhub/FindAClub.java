c35ec0125eb8263231acd0834586f39fcdd759f0
annalisaallan <aallan@wpi.edu>
2020-04-02 13:21:57 -0400

    find a club

---
+++ app/src/main/java/com/example/clubhub/FindAClub.java
@@ -0,0 +1,14 @@
+package com.example.clubhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindAClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_a_club);
+    }
+}
