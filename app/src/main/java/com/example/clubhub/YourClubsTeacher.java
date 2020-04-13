e7eea0cf2a9be366e08ee84075cc7c639bc01209
cliu11 <62902695+cliu11@users.noreply.github.com>
2020-04-13 15:01:52 -0400

    currently what i have for the teachers your club, will finish tonight. uploading incase computer loses power or lights don't come back. will also go back and do the missing classes for member, leader, etc.

--- app/src/main/java/com/example/clubhub/YourClubsTeacher.java
+++ app/src/main/java/com/example/clubhub/YourClubsTeacher.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class YourClubMember extends AppCompatActivity {
+public class YourClubsTeacher extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_your_club_member);
-        }
+        setContentView(R.layout.activity_your_clubs_teacher);
+    }
 }
