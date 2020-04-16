f646094713a608e95f3c4390616b289e7d02c07c
annalisaallan <aallan@wpi.edu>
2020-04-16 13:17:55 -0400

    advise new club- teachers

---
+++ app/src/main/java/com/example/clubhub/AdviseNewClubTeacher.java
@@ -0,0 +1,14 @@
+package com.example.clubhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdviseNewClubTeacher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advise_new_club_teacher);
+    }
+}
