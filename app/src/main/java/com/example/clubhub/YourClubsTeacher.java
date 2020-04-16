4088d27170f8f89b8a61c48a12c202cd8d138ebf
cliu11 <62902695+cliu11@users.noreply.github.com>
2020-04-16 14:17:44 -0400

    updated UI for the your clubs teacher page, working on the extra java code to get the recycler working rn

--- app/src/main/java/com/example/clubhub/YourClubsTeacher.java
+++ app/src/main/java/com/example/clubhub/YourClubsTeacher.java
@@ -1,8 +1,20 @@
 package com.example.clubhub;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ListView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class YourClubsTeacher extends AppCompatActivity {
 
